b04744366858632d05e45759aaaaa33d4ae16b37
tank1st99 <tank1st99@gmail.com>
2018-04-11 14:55:39 +0300

    Adding XXE lab (Code)

---
+++ labs/lab-01-xxe/src/main/java/edu/otib/lab01xxe/Lab01XxeApplication.java
@@ -0,0 +1,12 @@
+package edu.otib.lab01xxe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab01XxeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab01XxeApplication.class, args);
+	}
+}
